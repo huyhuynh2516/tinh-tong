7ef54eefa3e6a504ec3a760e31dfebfa1f37f1bb
huyhuynh2516 <huynhconghuy172004@gmail.com>
2022-12-25 17:23:37 +0700

    tinh tong

---
+++ tinh tong.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main (){
+	int a,b ;
+	cout << "nhap a,b " << endl;
+	cin >> a >> b ;
+	cout << "tong=" << a+b << endl;
+	return 0;
+}
